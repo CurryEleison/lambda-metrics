4006d368ba93b5e76310ca8b31bbe51901899277
Thomas Petersen <thomas@simplesite.com>
2016-12-27 16:00:08 +0000

    Improved layout and requirements

--- lambda/app.py
+++ lambda/app.py
@@ -1,5 +1,7 @@
 import logging
 
+# This is here because zappa and lambda really wants an end-point. The action is over in lambdametrics.py
+
 logging.basicConfig()
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -9,7 +11,7 @@ logger.setLevel(logging.INFO)
 def lambda_handler(event=None, context=None):
     logger.info('Lambda function invoked index()')
 
-    return 'hello from Flask'
+    return 'dummy endpoint hello'
 
 if __name__ == '__main__':
     app.run(debug=True)
